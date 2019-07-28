a71c9cfc5932ccb40391830a950e93616596d5bb
PrabhaAmbati <prabhareddy309@gmail.com>
2019-07-28 20:10:50 +0530

    dev-commit-1

--- simple-web-app/src/main/java/com/simple/webapp/simple_web_app/Sample.java
+++ simple-web-app/src/main/java/com/simple/webapp/simple_web_app/Sample.java
@@ -3,11 +3,6 @@ package com.simple.webapp.simple_web_app;
 public class Sample {
 public static void main(String args[]) {
 	System.out.println("kkk");
-	/*try {
-		System.exit(0);
-	}
-	finally {
-		System.out.println("hi");
-	}*/
+	System.out.println("this is practice class");
 }
 }
