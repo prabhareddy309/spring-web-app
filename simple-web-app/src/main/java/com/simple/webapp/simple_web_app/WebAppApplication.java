512dbbedcb9d6359e130917b7d58a7993d7568cc
PrabhaAmbati <prabhareddy309@gmail.com>
2019-07-28 13:03:50 +0530

    commit-3

--- simple-web-app/src/main/java/com/simple/webapp/simple_web_app/WebAppApplication.java
+++ simple-web-app/src/main/java/com/simple/webapp/simple_web_app/WebAppApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebAppApplication {
 	public static void main(String args[]) {
 	SpringApplication.run(WebAppApplication.class, args);
+	// this is in sync with git
 	}
 }
